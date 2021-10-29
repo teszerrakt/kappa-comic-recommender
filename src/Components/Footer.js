fcfc717c4726b74782825fecf790190ac0a3bc2c
teszerrakt <zaky.syihab@gmail.com>
2021-10-30 00:37:10 +0700

    open in new tab

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,12 @@ const Footer = () => {
     <div className="bottom-0 flex justify-center w-full bg-kappa-dark-gray text-kappa-gray">
       <h1 className="py-4 text-xl">
         Made with ❤️ by{" "}
-        <a href="http://zsyihab.tech" className="text-kappa-green">
+        <a
+          href="http://zsyihab.tech"
+          className="text-kappa-green"
+          target="_blank"
+          rel="noreferrer"
+        >
           Zaky Syihab
         </a>
       </h1>
