760cead1c1d3bcc98630e7a25a16c3e6babd19ef
Zaky Syihab Hatmoko <zaky.syihab@gmail.com>
2023-10-19 19:49:31 +0700

    fix: eslint error

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import fallbackImage from "../Assets/fallback-image.png";
 
-function Image({ src, ...props }) {
+function Image({ src, alt, ...props }) {
   const [isError, setIsError] = useState(false);
 
   if (isError) {
-    return <img src={fallbackImage} {...props} />;
+    return <img src={fallbackImage} alt={alt} {...props} />;
   }
 
-  return <img src={src} onError={() => setIsError(true)} {...props} />;
+  return (
+    <img src={src} alt={alt} onError={() => setIsError(true)} {...props} />
+  );
 }
 
 export default Image;
