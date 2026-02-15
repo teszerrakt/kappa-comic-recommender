77783008c6ed4b3cf98beb70865c2f28caf990e5
root <root@tzrrkt.cloud>
2026-02-15 18:35:11 +0000

    feat: send api token header

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,4 @@
 export const ALGOLIA_APP_ID: string = import.meta.env.VITE_ALGOLIA_APP_ID;
 export const ALGOLIA_API_KEY: string = import.meta.env.VITE_ALGOLIA_API_KEY;
 export const KAPPA_API_URL: string = import.meta.env.VITE_KAPPA_API_URL;
+export const KAPPA_API_TOKEN: string = import.meta.env.VITE_KAPPA_API_TOKEN;
